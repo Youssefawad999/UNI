3aee81751bd210b6e49083bd2684448b6619284b
Youssefawad999 <112219671+Youssefawad999@users.noreply.github.com>
2023-08-16 14:06:26 +0300

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -2,6 +2,64 @@
 #include <string>
 using namespace std;
 
+class Department
+{
+
+    private:
+    std::string name;
+    int capacity;
+    double requiredGpa;
+
+    public:
+
+    ///////constructors////////////
+    Department(){
+        std::string name="";
+    int capacity=0;
+    double requiredGpa=0.0;
+
+    }
+    Department(std::string dept_name;int dept_capacity;double dept_requiredGpa){
+       name=dept_name;
+       capacity=dept_capacity;
+       requiredGpa=dept_requiredGpa;
+
+    }
+    ///////////setters///////////////
+    void set_name(std::string dept_name)
+    {
+        name=dept_name;
+    }
+    void set_capacity(int dept_capacity)
+    {
+       capacity=dept_capacity;
+    }
+    void set_requiredGpa(double dept_requiredGpa )
+    {
+        requiredGpa=dept_requiredGpa;
+    }
+    //////////getters//////////////
+
+    std::string get_name() const
+    {
+        return name;
+
+    }
+    int  get_capacity() const
+    {
+        return capacity;
+
+    }
+    double get_requiredGpa() const
+    {
+        return requiredGpa;
+
+    }
+
+
+
+};
+
 
 class Student{
 
